1c6ea8688c19cde68fcd50dbf9ca474844c341d0
Anas <amqcomputer@gmail.com>
2015-06-24 14:26:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/anas/custom_launcher/AppDetail.java
@@ -0,0 +1,12 @@
+package com.example.anas.custom_launcher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Anas on 6/24/2015.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
